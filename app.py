b9a9432ae74560ba9619aedc8d4023e8cb2e939d
Mirela Iclodean <miclodean@twitter.com>
2018-04-08 10:18:21 +0100

    Setup my environment variables

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('MyHerokuApp')
 
@@ -8,6 +8,6 @@ mailgun_secret_key = os.environ.get('MAILGUN_SECRET_KEY', None)
 @app.route('/')
 def index():
     # We will just display our mailgun secret key, nothing more.
-    return render_template("index.html", mailgun_secret_key)
+    return render_template("index.html", key=mailgun_secret_key)
 
 app.run(debug=True)
